d421041d3ac537b9938f81f112d28c9d509350fa
Daniel Edwards <dredwards@email.wm.edu>
2020-03-28 14:31:28 -0400

    Daniel branch (#7)
    * added basic form with validation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { store } from './store';
 import AskContainer from './asks/AskContainer';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './layout/Header';
+import RequestForm from './asks/RequestForm';
 
 function App() {
   return (
@@ -25,7 +26,7 @@ function App() {
         )} />
         <Route exact path='/request' render={props => (
           <React.Fragment>
-            <p>Request Page</p>
+            <RequestForm/>
           </React.Fragment>
         )} />
       </div>
