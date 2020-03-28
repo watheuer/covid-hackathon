6bf0914e06d544a4a939c9ac6c2a5f59d1a6e171
Daniel Edwards <dredwards@email.wm.edu>
2020-03-28 12:19:02 -0400

    Daniel branch (#4)
    * added three pages and basic routing
    
    * updated header and added a logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import AskList from './asks/AskList';
+import Header from './layout/Header';
 
 function App() {
   return (
-    <Provider store={store}>
-      <AskList />
-    </Provider>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Route exact path='/' render={props =>(
+          <React.Fragment>
+            <p>Home Page</p>
+          </React.Fragment>
+        )} />
+        <Route exact path='/donate' render={props => (
+          <React.Fragment>
+            <p>Donate Page</p>
+            <Provider store={store}>
+              <AskList />
+            </Provider>
+          </React.Fragment>
+        )} />
+        <Route exact path='/request' render={props => (
+          <React.Fragment>
+            <p>Request Page</p>
+          </React.Fragment>
+        )} />
+      </div>
+    </Router>
+
   );
 }
 
