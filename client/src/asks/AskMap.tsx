508b6a232f26e6fe0bcee3f47ddcbdc9954d5813
William Theuer <watheuer@gmail.com>
2020-03-29 21:40:58 -0400

    Only update asks on map when the list changes

--- client/src/asks/AskMap.tsx
+++ client/src/asks/AskMap.tsx
@@ -42,50 +42,53 @@ export const AskMap: FunctionComponent<AskListProps> = ({ asks, fetching }) => {
         return () => window.removeEventListener('resize', listener);
     }, [map]);
 
-    asks.forEach(function (ask) {
-        let lngLat: LngLatLike;
-        let location = ask.location;
-        let address: string = location.street_address + " " + location.city + ", " +
-            location.state + " " + location.zip;
-        Geocoding({ accessToken: mapboxgl.accessToken })
-            .forwardGeocode({
-                query: address,
-                mode: "mapbox.places",
-                proximity: centerDefaultCoordinates as number[],
-                autocomplete: false,
-                limit: 1
-            })
-            .send()
-            .then(function (response) {
-                if (
-                    response &&
-                    response.body &&
-                    response.body.features &&
-                    response.body.features.length) {
-                    var feature = response.body.features[0];
+    // Rerender asks when the list changes
+    useEffect(() => {
+        asks.forEach(function (ask) {
+            let lngLat: LngLatLike;
+            let location = ask.location;
+            let address: string = location.street_address + " " + location.city + ", " +
+                location.state + " " + location.zip;
+            Geocoding({ accessToken: mapboxgl.accessToken })
+                .forwardGeocode({
+                    query: address,
+                    mode: "mapbox.places",
+                    proximity: centerDefaultCoordinates as number[],
+                    autocomplete: false,
+                    limit: 1
+                })
+                .send()
+                .then(function (response) {
+                    if (
+                        response &&
+                        response.body &&
+                        response.body.features &&
+                        response.body.features.length) {
+                        var feature = response.body.features[0];
 
-                    lngLat = feature.center;
-                    let popupHtml: string = `<strong>${ask.requester}</strong><p>Item: ${ask.item}<p>Address: ${address}<p>` +
-                        `Email: ${ask.email}<p>Phone: ${ask.phone}<p>Instructions: ${ask.instructions}`;
+                        lngLat = feature.center;
+                        let popupHtml: string = `<strong>${ask.requester}</strong><p>Item: ${ask.item}<p>Address: ${address}<p>` +
+                            `Email: ${ask.email}<p>Phone: ${ask.phone}<p>Instructions: ${ask.instructions}`;
 
-                    // create a HTML element for each feature
-                    var mapMarkerElement = document.createElement('div');
-                    mapMarkerElement.className = styles.mapMarker;
+                        // create a HTML element for each feature
+                        var mapMarkerElement = document.createElement('div');
+                        mapMarkerElement.className = styles.mapMarker;
 
-                    var popupElement = document.createElement('div');
-                    popupElement.className = styles.popup;
-                    let popup: mapboxgl.Popup = new mapboxgl.Popup()
-                        .setLngLat(lngLat)
-                        .setHTML(popupHtml)
-                        .addTo(map as mapboxgl.Map);
+                        var popupElement = document.createElement('div');
+                        popupElement.className = styles.popup;
+                        let popup: mapboxgl.Popup = new mapboxgl.Popup()
+                            .setLngLat(lngLat)
+                            .setHTML(popupHtml)
+                            .addTo(map as mapboxgl.Map);
 
-                    new mapboxgl.Marker(mapMarkerElement)
-                        .setLngLat(lngLat)
-                        .setPopup(popup)
-                        .addTo(map as mapboxgl.Map);
-                }
-            });
-    });
+                        new mapboxgl.Marker(mapMarkerElement)
+                            .setLngLat(lngLat)
+                            .setPopup(popup)
+                            .addTo(map as mapboxgl.Map);
+                    }
+                });
+        });
+    }, [asks]);
 
     return (<div className={styles.mapRoot} ref={mapContainer}></div>);
 }
